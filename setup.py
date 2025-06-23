ad0c602a478913dfceba50535d6c971336e982d7
Nastaran <nsaffar@gmail.com>
2025-06-24 09:25:52 +1200

    removing OpenCV dependencies

--- setup.py
+++ setup.py
@@ -9,14 +9,15 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='tobiiglassesctrl',
+    name='nastaran-tobiiglassesctrl', 
     version='2.4.3',
     description='A Python controller for Tobii Pro Glasses 2',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/ddetommaso/TobiiGlassesPyController/',
-    download_url='https://github.com/ddetommaso/TobiiGlassesPyController/archive/2.4.3.tar.gz',
-    install_requires=['netifaces', 'opencv-python==4.2.0.32', 'av==8.0.3'],
+    # Forked from'https://github.com/ddetommaso/TobiiGlassesPyController/',
+    url ='https://github.com/nastaran62/NastaranTobiiGlassesPyController/'
+ 
+    install_requires=['netifaces'],
     author='Davide De Tommaso',
     author_email='dtmdvd@gmail.com',
     keywords=['eye-tracker', 'tobii', 'glasses', 'tobii pro glasses 2', 'tobii glasses', 'eye tracking'],
@@ -26,6 +27,9 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',   
+        'Programming Language :: Python :: 3.12'
     ],
 )
